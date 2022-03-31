f8a98708aba9b90c0816e5f9892cd81bf77f77ef
Cookieman01 <13785517+Cookieman01@users.noreply.github.com>
2022-03-31 15:50:14 +0200

    Created Main

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from uis.python_files.mainwindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    main_window = MainWindow()
+    main_window.show()
+    app.exec_()
